2b546444154d1f246c7fd4cae7868760db113a80
nsp4746 <nikhilpat67@gmail.com>
2024-06-05 14:36:45 -0400

    Cloning to PC

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nikhil-p-03.netlify.app",
-  integrations: [preact()]
+  integrations: [preact(), mdx()]
 });
